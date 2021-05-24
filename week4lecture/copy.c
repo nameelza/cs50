6ed6ba7ac19eceee42b41331b86ce8537111af96
nameelza <lizasvirschik@gmail.com>
2021-05-24 22:25:27 +0000

    copy

--- week4lecture/copy.c
+++ week4lecture/copy.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 #include <cs50.h>
 #include <string.h>
+#include <stdlib.h>
 #include <ctype.h>
 
 int main(void)
 {
     char *s = get_string("s: ");
     
-    char *t = s;
+    char *t = malloc(strlen(s) + 1);
+    
+    for (int i = 0, n = strlen(s); i < n + 1; i++)
+    {
+        t[i] = s[i];
+    }
     
     t[0] = toupper(t[0]);
     
@@ -16,3 +22,4 @@ int main(void)
 }
 
 /// char * == string
+/// malloc = memory allocation
